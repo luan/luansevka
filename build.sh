f16c51a767ab42b2240f7b6ed43623d881a3bd52
Luan Santos <cfcluan@gmail.com>
2023-01-28 19:49:45 -0800

    🔖 release: build first version and patched

--- build.sh
+++ build.sh
@@ -1,13 +1,38 @@
 #!/usr/bin/env bash
 
-[ ! -f "Iosevka/package.json" ] && git submodule init && git submodule update
+if [ ! -f "Iosevka/package.json" ] || [ ! -f "nerd-fonts/package.json" ]; then
+   git submodule init && git submodule update
+fi
 
 [ ! -f "Iosevka/private-build-plans.toml" ] && ln build-plans.tom "Iosevka/private-build-plans.toml"
 
-(
-cd Iosevka || exit 1
-npm install
-npm run build -- ttf::luansevka
-npm run build -- ttf::luansevka-slab
-npm run build -- ttf::luansevka-ui
-)
+main() {
+   (
+   cd Iosevka || exit 1
+   npm install
+   npm run build -- ttf::luansevka
+   npm run build -- ttf::luansevka-slab
+   npm run build -- ttf::luansevka-ui
+   )
+
+   mkdir -p ./fonts/regular/luansevka{,-slab,-ui}
+   cp ./Iosevka/fonts/luansevka/ttf/*.ttf ./fonts/regular/luansevka/
+   cp ./Iosevka/fonts/luansevka-slab/ttf/*.ttf ./fonts/regular/luansevka-slab/
+   cp ./Iosevka/fonts/luansevka-ui/ttf/*.ttf ./fonts/regular/luansevka-ui/
+
+   (
+   cd nerd-fonts || exit 1
+   mkdir -p ../fonts/nerd-font/luansevka{,-slab,-ui}
+   find ../fonts/regular/luansevka/*.ttf -print0 | \
+      xargs -n 1 -I{} \
+      fontforge -script font-patcher "{}" -cls --outputdir=../fonts/nerd-font/luansevka/
+   find ../fonts/regular/luansevka-slab/*.ttf -print0 | \
+      xargs -n 1 -I{} \
+      fontforge -script font-patcher "{}" -cls --outputdir=../fonts/nerd-font/luansevka-slab/
+   find ../fonts/regular/luansevka-ui/*.ttf -print0 | \
+      xargs -n 1 -I{} \
+      fontforge -script font-patcher "{}" -cls --outputdir=../fonts/nerd-font/luansevka-ui/
+   )
+}
+
+main
